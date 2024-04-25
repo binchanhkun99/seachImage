5cd8c135ccecb40e5d4f255d362dad3d38894856
binchanhkun99 <hotrodhv58@gmail.com>
2024-04-25 15:30:44 +0700

    fix all

--- src/components/Navar.vue
+++ src/components/Navar.vue
@@ -61,14 +61,16 @@ const rdrHome = ()=>{
               <li  @click="closeSidebar"><svg class="toggle" xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0);"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"></path></svg></li>                <li><RouterLink
                 to="/"
                 class="app-logo d-flex align-center gap-x-2 app-title-wrapper"
-                >Trang chủ</RouterLink
+                @click="closeSidebar" >Trang chủ</RouterLink
               ></li>
                 <li><RouterLink
+                  @click="closeSidebar"
                 to="/about"
                 class="app-logo d-flex align-center gap-x-2 app-title-wrapper"
                 >Giới thiệu</RouterLink
               ></li>
                 <li><RouterLink
+                  @click="closeSidebar"
                 to="/event"
                 class="app-logo d-flex align-center gap-x-2 app-title-wrapper"
                 >Sự kiện</RouterLink
