395a310a1c7dceb13e653a1884a7ff33a53083a3
binchanhkun99 <hotrodhv58@gmail.com>
2024-05-27 18:44:08 +0700

    update codeeeee

---
+++ src/views/General.vue
@@ -0,0 +1,121 @@
+<script setup></script>
+<template>
+  <div class="about-container">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+          <div class="col t1">
+          <h1 class="fw-bold">ĐIỀU KHOẢN CHUNG</h1>
+          <p class="lead mt-4">
+Quy định chung
+Khi bạn truy cập và sử dụng trang web và các Dịch vụ liên quan, bạn đồng ý tuân thủ Điều khoản Sử dụng này, cũng như các chính sách của chúng tôi. Điều khoản sử dụng được tuân thủ và điều chỉnh theo pháp luật Việt Nam. Nếu một điều khoản bị vô hiệu sẽ không ảnh hưởng đến các điều khoản còn lại.
+
+Chúng tôi có thể điều chỉnh nội dung thông báo này bất cứ lúc nào bằng cách cập nhật lên website. Chúng tôi khuyến khích bạn thường xuyên kiểm tra trang web để cập nhật với các điều khoản và điều kiện mới khi sử dụng.
+
+Các điều khoản dịch vụ phản ánh cách thức hoạt động của Runpic.vn. Đây là những điều khoản mà chúng tôi tuân theo và áp dụng trong quá trình vận hành. Do đó, các điều khoản này giúp xác định mối quan hệ giữa Runpic.vn với bạn khi tương tác với các dịch vụ. 
+
+Runpic luôn luôn hoan nghênh những ý kiến/góp ý của bạn về nội dung website. Nếu có một phần nào đó của website này mà bạn cho rằng đã có dấu hiệu vi phạm bản quyền của bên thứ ba, vui lòng gửi liên hệ với chúng tôi qua email runpic.vn@gmail.com
+Bản quyền và thương hiệu
+Tuyên bố quyền sở hữu trí tuệ của Runpic với các thông tin thể hiện trên website như sau:
+Đây là trang website duy nhất của Runpic, chứa tất cả nội dung như văn bản, thiết kế, đồ họa, hình ảnh, giao diện, mã code đều thuộc bản quyền của Runpic hoặc bên thứ 3 cấp phép cho Runpic.
+Bất kỳ nội dung nào thuốc website này bao gồm hình ảnh, tên thương mại, mẫu logo,... đều thuộc quyền sở hữu của chúng tôi và được bảo hộ theo quy định của Luật sở hữu trí tuệ Việt Nam cũng như các văn bản liên quan.
+Mọi hành vi sao chép, lưu truyền,... vì mục đích thương mại dưới mọi hình thức mà không được sự chấp thuận của Runpic là xâm phạm quyền của chúng tôi. Chúng tôi có quyền yêu cầu người sử dụng chấm dứt việc xâm phạm và bồi thường thiệt hại (nếu có) theo quy định pháp luật.
+Quyền và trách nhiệm của Runpic
+Runpic có quyền thay đổi, thêm hoặc bớt các nội dung, tính năng của một hay toàn bộ website tùy vào thời điểm mà không thông báo trước.
+Runpic có quyền thực hiện các biện pháp an ninh để bảo vệ và chống các truy cập trái phép, sửa đổi trái phép, tiết lộ thông tin hoặc phá hủy thông tin.
+Khi sử dụng dịch vụ bằng phương tiện/thiết bị nằm ngoài sự kiểm soát của Runpic thì Runpic không chịu trách nhiệm cho bất kỳ sự chậm trễ, thất bại, bị gián đoạn của bất kỳ dữ liệu hoặc thông tin khác được truyền trong khi kết nối với việc sử dụng website.
+Runpic cam kết không chủ động tiết lộ bất kỳ thông tin nào của người sử dụng cho bên thứ ba, ngoại trừ trường hợp có yêu cầu bằng văn bản của các đơn vị điều tra theo đúng luật pháp hiện hành. Bạn phải ý thức và bảo đảm những thông tin, tài liệu mà bạn gửi phải nằm trong quyền hạn sử dụng của bạn; điều đó có nghĩa Runpic sẽ không vi phạm bất cứ quyền lợi nào của bên thứ ba.
+Quyền và trách nhiệm của người sử dụng
+Runpic cho phép người sử dụng xem, chiết xuất thông tin trên website (in, tải, chuyển tiếp…) hoặc chia sẻ cho người khác nhưng chỉ cho mục đích sử dụng cá nhân và phi thương mại với điều kiện phải trích dẫn thông báo bản quyền của chúng tôi.
+Bạn đảm bảo tuân theo pháp luật và các quy định liên quan đến việc sử dụng website của Runpic; Không can thiệp, gây ảnh hưởng đến việc sử dụng website của những người sử dụng khác; Không can thiệp vào hoạt động và quản lý website của Runpic.
+Bạn nhận thức rõ và chấp nhận rằng Runpic và/hoặc các công ty thành viên/đơn vị trực thuộc /nhân viên Runpic không chịu trách nhiệm đối với bất kỳ tổn thất, thiệt hại, chi phí phát sinh từ bất kỳ quyết định nào của bạn khi sử dụng bất kỳ thông tin nào trên website với bất kỳ nguyên nhân gì.
+Nếu bạn không bằng lòng với bất kỳ thông tin nào trên Website hoặc với bất kỳ điều khoản và điều kiện sử dụng thông tin trên Website này thì phương thức duy nhất bạn nên thực hiện là chấm dứt truy cập/sử dụng thông tin trên website.
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  </template>
+
+<style scoped>
+.about-container {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  overflow: hidden;
+}
+
+.bg-light {
+  --bs-bg-opacity: 1;
+  --bs-light-rgb: 248, 249, 250;
+  background-color: rgba(var(--bs-light-rgb), var(--bs-bg-opacity)) !important;
+}
+.container {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: calc(var(--bs-gutter-x) * 0.5);
+  padding-left: calc(var(--bs-gutter-x) * 0.5);
+}
+.col {
+  flex: 1 0 0%;
+}
+.fw-bold {
+  font-weight: 700;
+}
+.lead {
+  font-size: 1.25rem;
+  font-weight: 300;
+}
+.mt-4 {
+  margin-top: 1.5rem;
+}
+.d-flex {
+  display: flex;
+}
+.align-items-center {
+  align-items: center;
+}
+.row {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  flex-wrap: wrap;
+  margin-top: calc(-1 * var(--bs-gutter-y));
+  margin-right: calc(-0.5 * var(--bs-gutter-x));
+  margin-left: calc(-0.5 * var(--bs-gutter-x));
+}
+.text-end {
+  text-align: right;
+}
+.about-container {
+  margin-top: 38px;
+}
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1140px;
+  }
+  h1 {
+    font-size: 2.5rem;
+  }
+}
+@media (max-width: 991px) {
+  .row .col {
+    flex-direction: column;
+  }
+  .row {
+    display: flex;
+    flex-direction: column;
+  }
+  .t1 {
+    width: 80%;
+  }
+  .img-first {
+    width: 423px;
+    margin-right: 100px;
+  }
+  .i1 {
+    margin-right: -123px;
+  }
+}
+</style>
---
+++ src/views/Privacy.vue
@@ -0,0 +1,127 @@
+<script setup></script>
+<template>
+  <div class="about-container">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+          <div class="col t1">
+          <h1 class="fw-bold">CHÍNH SÁCH BẢO MẬT</h1>
+          <p class="lead mt-4">
+Mục đích và Phạm vi
+Mục đích của chính sách này là bảo vệ thông tin cá nhân của khách hàng, đối tác và nhân viên của Runpic (sau đây gọi là "Chúng tôi", "Của chúng tôi").
+Chính sách áp dụng cho mọi thông tin cá nhân mà chúng tôi thu thập, lưu trữ, xử lý và chia sẻ trong quá trình hoạt động kinh doanh của mình.
+Thu thập thông tin cá nhân
+Chúng tôi chỉ thu thập thông tin cá nhân cần thiết và có mục đích từ các cá nhân theo cách công bằng và hợp pháp.
+Các phương tiện thu thập thông tin bao gồm mẫu đăng ký, trang web, cuộc trò chuyện trực tuyến, email, cuộc trao đổi trực tiếp và các phương tiện khác có thể được sử dụng trong hoạt động kinh doanh.
+Loại thông tin cá nhân thu thập
+Thông tin cá nhân có thể bao gồm tên, địa chỉ, số điện thoại, địa chỉ email, ngày sinh, thông tin tài khoản ngân hàng, thông tin về sản phẩm và dịch vụ, và bất kỳ thông tin nào khác cần thiết cho mục đích cụ thể.
+Sử dụng thông tin Cá nhân
+Thông tin cá nhân sẽ chỉ được sử dụng cho mục đích đã được quy định và thông báo cho cá nhân liên quan.
+Mục đích sử dụng thông tin có thể bao gồm cung cấp sản phẩm và dịch vụ, xử lý thanh toán, hỗ trợ khách hàng, quản lý tài khoản, tiếp thị và quảng cáo, và tuân thủ yêu cầu pháp lý.
+Bảo mật thông tin
+Chúng tôi cam kết bảo vệ thông tin cá nhân bằng cách thực hiện biện pháp kỹ thuật, tổ chức và hành vi phù hợp.
+Các biện pháp bảo mật bao gồm mã hóa dữ liệu, kiểm soát truy cập, giám sát hệ thống, đào tạo nhân viên và thực hiện các tiêu chuẩn bảo mật.
+Chia sẻ thông tin
+Chúng tôi chỉ chia sẻ thông tin cá nhân với bên thứ ba khi cần thiết để hoàn thành mục đích đã quy định và sau khi đã nhận được sự đồng ý của cá nhân liên quan.
+Các bên thứ ba này có thể bao gồm nhà cung cấp dịch vụ, đối tác kinh doanh, cơ quan chính phủ hoặc các bên khác có quyền theo luật.
+Bảo quản và xóa thông tin
+Chúng tôi sẽ bảo quản thông tin cá nhân chỉ trong thời gian cần thiết cho mục đích đã quy định hoặc cho đến khi cá nhân liên quan yêu cầu xóa thông tin.
+Chúng tôi sẽ xoá hoặc vô hiệu hóa thông tin cá nhân khi không còn cần thiết và theo yêu cầu của cá nhân.
+Quyền lợi của cá nhân
+Cá nhân có quyền truy cập, sửa đổi hoặc xóa thông tin cá nhân của mình.
+Cá nhân cũng có quyền yêu cầu chúng tôi ngừng sử dụng hoặc chia sẻ thông tin cá nhân của họ.
+Tuân thủ pháp luật
+Chúng tôi cam kết tuân thủ mọi quy định pháp luật liên quan đến bảo vệ thông tin cá nhân, bao gồm GDPR, CCPA và các quy định bảo vệ dữ liệu khác.
+Đánh giá và cập nhật
+Chúng tôi sẽ đánh giá và cập nhật chính sách Bảo mật Thông tin này định kỳ để đảm bảo rằng nó vẫn phản ánh các tiêu chuẩn và thực tiễn tốt nhất.
+
+Nếu có bất kỳ câu hỏi hoặc mối quan ngại nào về việc bảo mật thông tin cá nhân, xin vui lòng liên hệ với chúng tôi qua email runpic.vn@gmail.com
+
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  </template>
+
+<style scoped>
+.about-container {
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  overflow: hidden;
+}
+
+.bg-light {
+  --bs-bg-opacity: 1;
+  --bs-light-rgb: 248, 249, 250;
+  background-color: rgba(var(--bs-light-rgb), var(--bs-bg-opacity)) !important;
+}
+.container {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: calc(var(--bs-gutter-x) * 0.5);
+  padding-left: calc(var(--bs-gutter-x) * 0.5);
+}
+.col {
+  flex: 1 0 0%;
+}
+.fw-bold {
+  font-weight: 700;
+}
+.lead {
+  font-size: 1.25rem;
+  font-weight: 300;
+}
+.mt-4 {
+  margin-top: 1.5rem;
+}
+.d-flex {
+  display: flex;
+}
+.align-items-center {
+  align-items: center;
+}
+.row {
+  --bs-gutter-x: 1.5rem;
+  --bs-gutter-y: 0;
+  flex-wrap: wrap;
+  margin-top: calc(-1 * var(--bs-gutter-y));
+  margin-right: calc(-0.5 * var(--bs-gutter-x));
+  margin-left: calc(-0.5 * var(--bs-gutter-x));
+}
+.text-end {
+  text-align: right;
+}
+.about-container {
+  margin-top: 38px;
+}
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1140px;
+  }
+  h1 {
+    font-size: 2.5rem;
+  }
+}
+@media (max-width: 991px) {
+  .row .col {
+    flex-direction: column;
+  }
+  .row {
+    display: flex;
+    flex-direction: column;
+  }
+  .t1 {
+    width: 80%;
+  }
+  .img-first {
+    width: 423px;
+    margin-right: 100px;
+  }
+  .i1 {
+    margin-right: -123px;
+  }
+}
+</style>
