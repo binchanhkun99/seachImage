559375d03dd8c7b30b1db85b66c3e39834afb690
binchanhkun99 <hotrodhv58@gmail.com>
2024-05-27 20:07:51 +0700

    Update css tw file

--- src/views/General.vue
+++ src/views/General.vue
@@ -6,29 +6,21 @@
           <div class="col t1">
           <h1 class="fw-bold">ĐIỀU KHOẢN CHUNG</h1>
           <p class="lead mt-4">
-Quy định chung
-Khi bạn truy cập và sử dụng trang web và các Dịch vụ liên quan, bạn đồng ý tuân thủ Điều khoản Sử dụng này, cũng như các chính sách của chúng tôi. Điều khoản sử dụng được tuân thủ và điều chỉnh theo pháp luật Việt Nam. Nếu một điều khoản bị vô hiệu sẽ không ảnh hưởng đến các điều khoản còn lại.
-
-Chúng tôi có thể điều chỉnh nội dung thông báo này bất cứ lúc nào bằng cách cập nhật lên website. Chúng tôi khuyến khích bạn thường xuyên kiểm tra trang web để cập nhật với các điều khoản và điều kiện mới khi sử dụng.
-
-Các điều khoản dịch vụ phản ánh cách thức hoạt động của Runpic.vn. Đây là những điều khoản mà chúng tôi tuân theo và áp dụng trong quá trình vận hành. Do đó, các điều khoản này giúp xác định mối quan hệ giữa Runpic.vn với bạn khi tương tác với các dịch vụ. 
-
-Runpic luôn luôn hoan nghênh những ý kiến/góp ý của bạn về nội dung website. Nếu có một phần nào đó của website này mà bạn cho rằng đã có dấu hiệu vi phạm bản quyền của bên thứ ba, vui lòng gửi liên hệ với chúng tôi qua email runpic.vn@gmail.com
-Bản quyền và thương hiệu
-Tuyên bố quyền sở hữu trí tuệ của Runpic với các thông tin thể hiện trên website như sau:
-Đây là trang website duy nhất của Runpic, chứa tất cả nội dung như văn bản, thiết kế, đồ họa, hình ảnh, giao diện, mã code đều thuộc bản quyền của Runpic hoặc bên thứ 3 cấp phép cho Runpic.
-Bất kỳ nội dung nào thuốc website này bao gồm hình ảnh, tên thương mại, mẫu logo,... đều thuộc quyền sở hữu của chúng tôi và được bảo hộ theo quy định của Luật sở hữu trí tuệ Việt Nam cũng như các văn bản liên quan.
-Mọi hành vi sao chép, lưu truyền,... vì mục đích thương mại dưới mọi hình thức mà không được sự chấp thuận của Runpic là xâm phạm quyền của chúng tôi. Chúng tôi có quyền yêu cầu người sử dụng chấm dứt việc xâm phạm và bồi thường thiệt hại (nếu có) theo quy định pháp luật.
-Quyền và trách nhiệm của Runpic
-Runpic có quyền thay đổi, thêm hoặc bớt các nội dung, tính năng của một hay toàn bộ website tùy vào thời điểm mà không thông báo trước.
-Runpic có quyền thực hiện các biện pháp an ninh để bảo vệ và chống các truy cập trái phép, sửa đổi trái phép, tiết lộ thông tin hoặc phá hủy thông tin.
-Khi sử dụng dịch vụ bằng phương tiện/thiết bị nằm ngoài sự kiểm soát của Runpic thì Runpic không chịu trách nhiệm cho bất kỳ sự chậm trễ, thất bại, bị gián đoạn của bất kỳ dữ liệu hoặc thông tin khác được truyền trong khi kết nối với việc sử dụng website.
-Runpic cam kết không chủ động tiết lộ bất kỳ thông tin nào của người sử dụng cho bên thứ ba, ngoại trừ trường hợp có yêu cầu bằng văn bản của các đơn vị điều tra theo đúng luật pháp hiện hành. Bạn phải ý thức và bảo đảm những thông tin, tài liệu mà bạn gửi phải nằm trong quyền hạn sử dụng của bạn; điều đó có nghĩa Runpic sẽ không vi phạm bất cứ quyền lợi nào của bên thứ ba.
-Quyền và trách nhiệm của người sử dụng
-Runpic cho phép người sử dụng xem, chiết xuất thông tin trên website (in, tải, chuyển tiếp…) hoặc chia sẻ cho người khác nhưng chỉ cho mục đích sử dụng cá nhân và phi thương mại với điều kiện phải trích dẫn thông báo bản quyền của chúng tôi.
-Bạn đảm bảo tuân theo pháp luật và các quy định liên quan đến việc sử dụng website của Runpic; Không can thiệp, gây ảnh hưởng đến việc sử dụng website của những người sử dụng khác; Không can thiệp vào hoạt động và quản lý website của Runpic.
-Bạn nhận thức rõ và chấp nhận rằng Runpic và/hoặc các công ty thành viên/đơn vị trực thuộc /nhân viên Runpic không chịu trách nhiệm đối với bất kỳ tổn thất, thiệt hại, chi phí phát sinh từ bất kỳ quyết định nào của bạn khi sử dụng bất kỳ thông tin nào trên website với bất kỳ nguyên nhân gì.
-Nếu bạn không bằng lòng với bất kỳ thông tin nào trên Website hoặc với bất kỳ điều khoản và điều kiện sử dụng thông tin trên Website này thì phương thức duy nhất bạn nên thực hiện là chấm dứt truy cập/sử dụng thông tin trên website.
+<p>Quy định chung</p>
+<p>Khi bạn truy cập và sử dụng trang web và các Dịch vụ liên quan, bạn đồng ý tuân thủ Điều khoản Sử dụng này, cũng như các chính sách của chúng tôi. Điều khoản sử dụng được tuân thủ và điều chỉnh theo pháp luật Việt Nam. Nếu một điều khoản bị vô hiệu sẽ không ảnh hưởng đến các điều khoản còn lại.</p>
+<p>Chúng tôi có thể điều chỉnh nội dung thông báo này bất cứ lúc nào bằng cách cập nhật lên website. Chúng tôi khuyến khích bạn thường xuyên kiểm tra trang web để cập nhật với các điều khoản và điều kiện mới khi sử dụng.</p>
+<p>Các điều khoản dịch vụ phản ánh cách thức hoạt động của Runpic.vn. Đây là những điều khoản mà chúng tôi tuân theo và áp dụng trong quá trình vận hành. Do đó, các điều khoản này giúp xác định mối quan hệ giữa Runpic.vn với bạn khi tương tác với các dịch vụ.</p>
+<p>Runpic luôn luôn hoan nghênh những ý kiến/góp ý của bạn về nội dung website. Nếu có một phần nào đó của website này mà bạn cho rằng đã có dấu hiệu vi phạm bản quyền của bên thứ ba, vui lòng gửi liên hệ với chúng tôi qua email runpic.vn@gmail.com</p>
+<p>Bản quyền và thương hiệu</p>
+<p>Tuyên bố quyền sở hữu trí tuệ của Runpic với các thông tin thể hiện trên website như sau: Đây là trang website duy nhất của Runpic, chứa tất cả nội dung như văn bản, thiết kế, đồ họa, hình ảnh, giao diện, mã code đều thuộc bản quyền của Runpic hoặc bên thứ 3 cấp phép cho Runpic. Bất kỳ nội dung nào thuốc website này bao gồm hình ảnh, tên thương mại, mẫu logo,... đều thuộc quyền sở hữu của chúng tôi và được bảo hộ theo quy định của Luật sở hữu trí tuệ Việt Nam cũng như các văn bản liên quan.</p>
+<p>Mọi hành vi sao chép, lưu truyền,... vì mục đích thương mại dưới mọi hình thức mà không được sự chấp thuận của Runpic là xâm phạm quyền của chúng tôi. Chúng tôi có quyền yêu cầu người sử dụng chấm dứt việc xâm phạm và bồi thường thiệt hại (nếu có) theo quy định pháp luật.</p>
+<p>Quyền và trách nhiệm của Runpic</p>
+<p>Runpic có quyền thay đổi, thêm hoặc bớt các nội dung, tính năng của một hay toàn bộ website tùy vào thời điểm mà không thông báo trước. Runpic có quyền thực hiện các biện pháp an ninh để bảo vệ và chống các truy cập trái phép, sửa đổi trái phép, tiết lộ thông tin hoặc phá hủy thông tin.</p>
+<p>Khi sử dụng dịch vụ bằng phương tiện/thiết bị nằm ngoài sự kiểm soát của Runpic thì Runpic không chịu trách nhiệm cho bất kỳ sự chậm trễ, thất bại, bị gián đoạn của bất kỳ dữ liệu hoặc thông tin khác được truyền trong khi kết nối với việc sử dụng website.</p>
+<p>Runpic cam kết không chủ động tiết lộ bất kỳ thông tin nào của người sử dụng cho bên thứ ba, ngoại trừ trường hợp có yêu cầu bằng văn bản của các đơn vị điều tra theo đúng luật pháp hiện hành. Bạn phải ý thức và bảo đảm những thông tin, tài liệu mà bạn gửi phải nằm trong quyền hạn sử dụng của bạn; điều đó có nghĩa Runpic sẽ không vi phạm bất cứ quyền lợi nào của bên thứ ba.</p>
+<p>Quyền và trách nhiệm của người sử dụng</p>
+<p>Runpic cho phép người sử dụng xem, chiết xuất thông tin trên website (in, tải, chuyển tiếp…) hoặc chia sẻ cho người khác nhưng chỉ cho mục đích sử dụng cá nhân và phi thương mại với điều kiện phải trích dẫn thông báo bản quyền của chúng tôi. Bạn đảm bảo tuân theo pháp luật và các quy định liên quan đến việc sử dụng website của Runpic; Không can thiệp, gây ảnh hưởng đến việc sử dụng website của những người sử dụng khác; Không can thiệp vào hoạt động và quản lý website của Runpic.</p>
+<p>Bạn nhận thức rõ và chấp nhận rằng Runpic và/hoặc các công ty thành viên/đơn vị trực thuộc /nhân viên Runpic không chịu trách nhiệm đối với bất kỳ tổn thất, thiệt hại, chi phí phát sinh từ bất kỳ quyết định nào của bạn khi sử dụng bất kỳ thông tin nào trên website với bất kỳ nguyên nhân gì. Nếu bạn không bằng lòng với bất kỳ thông tin nào trên Website hoặc với bất kỳ điều khoản và điều kiện sử dụng thông tin trên Website này thì phương thức duy nhất bạn nên thực hiện là chấm dứt truy cập/sử dụng thông tin trên website.</p>
 
           </p>
         </div>
@@ -70,6 +62,7 @@ Nếu bạn không bằng lòng với bất kỳ thông tin nào trên Website h
 }
 .mt-4 {
   margin-top: 1.5rem;
+  text-align: justify;
 }
 .d-flex {
   display: flex;
